1bc040d941ea1ce8c95634f30692a4aaca37d00b
Victor Vergel <victorvergel@verinsis.com>
2025-03-12 13:31:03 +0100

    Actualiacion de hola mundo2

--- Ejem21_ConfiguracionGitHub/src/main/java/Aplicacion.java
+++ Ejem21_ConfiguracionGitHub/src/main/java/Aplicacion.java
@@ -4,6 +4,7 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo 2");
 	}
 
 }
